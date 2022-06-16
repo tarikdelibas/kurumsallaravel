8eb0cd04833434a1d9c08ed2a978c5858b30e498
tarikdelibas <tarik1990@gmail.com>
2022-06-17 00:36:12 +0300

    deneme1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Deneme
+                    Laravel Deneme1111
                 </div>
 
                 <div class="links">
